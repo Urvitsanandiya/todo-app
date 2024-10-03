b3203e4c9c66921e26cbe14860748f56bc957c3d
Urvitsanandiya <161760540+Urvitsanandiya@users.noreply.github.com>
2024-10-03 16:34:52 +0530

    Add

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:3000}
-})
+  server: {
+    host: "0.0.0.0", // Allows external connections, which is important for production
+    port: process.env.PORT || 3000, // Use the PORT provided by the environment or fallback to 3000 for local dev
+  },
+});
